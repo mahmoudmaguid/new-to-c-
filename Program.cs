4aee34de992172cb3e92838d38fd1a9d19d7bbb0
Mahmoud <bombolinii@gmail.com>
2020-02-12 11:38:19 -0700

    new to c#

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace c_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 23;
+ //  PANNYET FOR START TO END
+        //     Console.WriteLine(" HELLO PANNYET!"+X);
+        //  if ( X<30 )
+        //  {
+        //      Console.WriteLine ("X IS LESS THAN THIRTY");
+        //  } 
+        //  else if( X==30)
+        //  {
+        //      Console.WriteLine("x is thirty");
+
+        //  }
+        //   else {
+        //       Console.WriteLine(" x is greater than thirty");
+        int a=3;
+        switch (a) {
+            case 1 :
+            Console.WriteLine("value is 1!");
+            break;
+            case 2 :
+            Console.WriteLine ("value is 2!");
+            break;
+            case 3 :
+            Console.WriteLine ("value is 3 !");
+            break;
+            default:
+            break;
+        }
+      }
+
+
+
+       }
+    }
+
+
+
+
+      
+    
+        
+ 
